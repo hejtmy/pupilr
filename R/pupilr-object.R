45d158d74bbadeccdf558c3b1977b155dd603e4d
hejtmy <hejtmy@gmail.com>
2019-08-20 14:51:19 +0200

    Adds surface definition

--- R/pupilr-object.R
+++ R/pupilr-object.R
@@ -8,6 +8,22 @@
 PupilrObject <- function(){
   ls <- list()
   ls$data <- list()
+  ls$info <- list()
+  ls$surfaces <- list()
   class(ls) <- append(class(ls), "pupilr")
+  return(ls)
+}
+
+#' Creates surface object
+#'
+#' @return
+#' @export
+#'
+#' @examples
+SurfaceObject <- function(){
+  ls <- list()
+  ls$data <- list()
+  ls$items <- list()
+  class(ls) <-append(class(ls), "surface")
   return(ls)
 }
