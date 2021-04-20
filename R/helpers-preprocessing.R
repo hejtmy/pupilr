d31706cd6286d5ee29460242e69359009e1fd2ab
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-04-20 03:02:38 +0200

    adds downsampling function

--- R/helpers-preprocessing.R
+++ R/helpers-preprocessing.R
@@ -2,3 +2,10 @@ rename_column <- function(df, old_column, new_column){
   colnames(df)[colnames(df) %in% c(old_column)] <- new_column
   return(df)
 }
+
+downsample <- function(df, n){
+  if(n < 2 ) return(df)
+  if(nrow(df) < 1) return(df)
+  df <- df[seq(1, nrow(df), n), ]
+  return(df)
+}
