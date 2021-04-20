11496ba466300d2705db06fcfe0a1dbf93ae0065
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-04-20 03:02:24 +0200

    small updates

--- R/helpers-loading.R
+++ R/helpers-loading.R
@@ -1,11 +1,11 @@
 load_exported_file <- function(path){
   # validate path
-  df <- read.table(path, sep = ",", header = T, stringsAsFactors = F)
+  df <- read.table(path, sep = ",", header = TRUE, stringsAsFactors = FALSE)
   return(df)
 }
 
 find_single_file <- function(dir, ptr){
-  ls <- list.files(dir, ptr, full.names = T)
+  ls <- list.files(dir, ptr, full.names = TRUE)
   if(length(ls) < 1 ) warning("There are no files of pattern ", ptr, " in directory ", dir)
   if(length(ls) > 1 ) warning("There are ", length(ls), " files of pattern ", ptr, " in directory ", dir)
   if(length(ls) == 1) return(ls[1])
